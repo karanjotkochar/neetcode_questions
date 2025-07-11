84a35c0080372f00e5fe5e0d01f5d3d9024afe85
karanjotkochar <karanjot.kochar@gmail.com>
2025-07-11 16:01:43 -0400

    add q19

---
+++ q19_reverseLinkedList.java
@@ -0,0 +1,31 @@
+public class q19_reverseLinkedList {
+
+    public static void main(String[] args) {
+    }
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode temp;
+
+        while(current != null) {
+            temp = current.next;
+            current.next = prev;
+
+            prev = current;
+            current = temp;
+        }
+
+        return prev;
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
+}
