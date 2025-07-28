fcba4e8470fa08a3ad4075d032449f6c7f060994
karanjotkochar <karanjot.kochar@gmail.com>
2025-07-28 06:30:29 -0400

    add q33 v2

--- q33_RemoveNodeEndLinkedList.java
+++ q33_RemoveNodeEndLinkedList.java
@@ -4,6 +4,7 @@ public class q33_RemoveNodeEndLinkedList {
         int n = 2;
         print(ls);
         removeNthFromEnd(ls, n);
+        removeNthFromEnd2(ls, n);
         print(ls);
     }
     public static ListNode removeNthFromEnd(ListNode head, int n) {
@@ -32,6 +33,34 @@ public class q33_RemoveNodeEndLinkedList {
         }
 
         return head;
+    }
+
+    public static ListNode removeNthFromEnd2(ListNode head, int n) {
+        // two pointer
+        // time = o(n) and space = o(1)
+
+        // dummy node to start from ), keeping one position back
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode left = dummy;
+        ListNode right = head;
+
+        // right reaches end of Linked List
+        while (n>0) {
+            right = right.next;
+            n--;
+        }
+
+        // right is at end and left is one position back to the nth node
+        while (right != null) {
+            left = left.next;
+            right = right.next;
+        }
+
+        left.next = left.next.next;
+
+        return dummy.next;
     }
 
     public static void print(ListNode head) {
