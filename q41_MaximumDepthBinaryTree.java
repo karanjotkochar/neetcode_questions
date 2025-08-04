a6cb4036bfe7c3599ae2ccfac1b995e1d49db870
Karanjot Singh Kochar <karanjot.kochar@gmail.com>
2025-08-04 11:43:21 -0400

    add q41--v2

--- q41_MaximumDepthBinaryTree.java
+++ q41_MaximumDepthBinaryTree.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class q41_MaximumDepthBinaryTree {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
@@ -6,6 +10,7 @@ public class q41_MaximumDepthBinaryTree {
         root.right.left = new TreeNode(4);
 
         System.out.println(maxDepth(root));
+        System.out.println(maxDepth2(root));
     }
     public static int maxDepth(TreeNode root) {
         // DFS
@@ -18,6 +23,31 @@ public class q41_MaximumDepthBinaryTree {
         int right = maxDepth(root.right);
 
         return Math.max(left,right) + 1;
+    }
+    public static int maxDepth2(TreeNode root) {
+        // Breadth First Search
+        // time = o(n) and space = o(n)
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) {
+            q.add(root);                                    // add root to queue : begin level calculation
+        }
+
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();                            // size of current queue to iterate
+
+            for (int i=0; i<size; i++) {
+                TreeNode node = q.poll();                   // remove node from left: poll
+
+                if (node.left != null)                      // add to right: add and calculate = previous level
+                    q.add(node.left);
+
+                if (node.right != null)
+                    q.add(node.right);
+            }
+            level++;
+        }
+        return level;
     }
 
 }
