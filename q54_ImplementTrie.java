a57172d7ebc046d728c0d7ed2fec473e60cc7686
karanjotkochar <karanjot.kochar@gmail.com>
2026-01-16 13:31:58 +0100

    q54: add comments

--- q54_ImplementTrie.java
+++ q54_ImplementTrie.java
@@ -1,16 +1,21 @@
 class TrieNode {
+    // each node has 26 children (Array) and end of word tells completion of word
     TrieNode[] children = new TrieNode[26];
     boolean endOfWord = false;
 }
 
 public class q54_ImplementTrie {
+    // time = O(n) for each function, n = length of string
+    // space = o(t), t = total no of nodes(TrieNodes) created
     private TrieNode root;
 
     public q54_ImplementTrie() {
+        // initialise root node
         root = new TrieNode();
     }
 
     public void insert(String word) {
+        // initialise curr (children array) and start from assigning to root and traverse till end
         TrieNode curr = root;
         for (char c : word.toCharArray()) {
             int i = c - 'a';
@@ -23,6 +28,8 @@ public class q54_ImplementTrie {
     }
 
     public boolean search(String word) {
+        // initialise curr (children array) and start from assigning to root and traverse till end
+        // check if end of word is true
         TrieNode curr = root;
         for (char c : word.toCharArray()) {
             int i = c - 'a';
@@ -35,6 +42,7 @@ public class q54_ImplementTrie {
     }
 
     public boolean startsWith(String prefix) {
+        // check for all characters and return true/false
         TrieNode curr = root;
         for (char c : prefix.toCharArray()) {
             int i = c - 'a';
