6d2c2a95d1b64b509caf1621f33935aa92816556
karanjotkochar <karanjot.kochar@gmail.com>
2026-01-21 23:37:36 +0100

    q59: add v1

---
+++ q59_SingleNumber.java
@@ -0,0 +1,19 @@
+public class q59_SingleNumber {
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,3};
+        singleNumber(nums);
+        System.out.println(singleNumber(nums));
+    }
+    public static int singleNumber(int[] nums) {
+        // add comments
+
+        int result = 0;
+
+        for (int num : nums) {
+            result = result ^ num;
+        }
+
+        return result;
+    }
+}
