bb7115a2f612493172502377dcdb7d12026e4089
karanjotkochar <karanjot.kochar@gmail.com>
2026-02-17 01:20:31 +0100

    q66: add v0

--- Q66_Twitter.java
+++ Q66_Twitter.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Q66_Twitter {
+import java.util.List;
+
+public class Q66_Twitter {
+
+    public Q66_Twitter() {
+
+    }
+
+    public void postTweet(int userId, int tweetId) {
+
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+
+    }
+
+    public void follow(int followerId, int followeeId) {
+
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+
+    }
 }
