8ff3ef8c6bee98b01228ff42147fcf1adecfdfe7
karanjotkochar <karanjot.kochar@gmail.com>
2026-02-17 15:20:28 +0100

    q65: add v1

--- q65_TaskScheduler.java
+++ q65_TaskScheduler.java
@@ -9,37 +9,40 @@ public class q65_TaskScheduler {
     }
 
     public static int leastInterval(char[] tasks, int n) {
+        // Max heap or Priority Queue
+        // TC = O(n)
+        // Space = O(1)
 
-        int[] count = new int[26];
+        int[] count = new int[26];          // make a integer array
 
-        for (char task : tasks) {
+        for (char task : tasks) {           // make a frequency of chars
             count[task - 'A']++;
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pqHeap = new PriorityQueue<>(Collections.reverseOrder());    // max Heap
         for (int cnt : count) {
             if (cnt > 0) {
-                pq.offer(cnt);
+                pqHeap.offer(cnt);
             }
         }
 
         int time = 0;
         Queue<int[]> q = new LinkedList<>();
-
-        while (!pq.isEmpty() || !q.isEmpty()) {
+        while (!pqHeap.isEmpty() || !q.isEmpty()) {
             time++;
 
-            if (pq.isEmpty()) {
-                time = q.peek()[1];
+            if (pqHeap.isEmpty()) {                                     // Heap is empty and Queue is not empty
+                time = q.peek()[1];                                     // So, next time == next available time (** this does the skip-ing **)
             } else {
-                int currTask = pq.poll() - 1;                            // take the element
+
+                int currTask = pqHeap.poll() - 1;                       // take the element
                 if (currTask > 0) {
-                    q.add(new int[]{currTask, time + n});                // reduce by 1 and add the element to cooling queue
+                    q.add(new int[]{currTask, time + n});               // reduce by 1 and add the element to cooling queue
                 }
             }
 
             if (!q.isEmpty() && q.peek()[1] == time) {                  // check queue: when item is ready from the cooling queue
-                pq.add(q.poll()[0]);                                    // add elemt back to max heap : FIFO
+                pqHeap.add(q.poll()[0]);                                // add elemt back to max heap : FIFO
             }
         }
 
