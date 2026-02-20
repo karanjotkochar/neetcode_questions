0bf944b10bae00b48e0190af70478780b8f00a8b
karanjotkochar <karanjot.kochar@gmail.com>
2026-02-20 21:50:20 +0100

    q71: add v1

--- q71_MaximumSubarray.java
+++ q71_MaximumSubarray.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class q71_MaximumSubarray {
+public class q71_MaximumSubarray {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,-3,4,-2,2,1,-1,4};
+        maxSubArray(nums);
+    }
+
+    public static int maxSubArray(int[] nums) {
+        // Maximum Subarray Sum - Kadane's Algorithm
+        // TC = O(n)
+        // SC = O(1)
+
+        int currSum = 0;
+        int maxSub = nums[0];
+
+        for (int i : nums) {
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            currSum = currSum + i;
+            maxSub = Math.max(maxSub, currSum);
+        }
+
+        return maxSub;
+    }
+
 }
