70c7f068ca2f9662c46e139b12183695ceb44b96
karanjotkochar <karanjot.kochar@gmail.com>
2026-02-28 01:25:45 +0100

    q80: add v2

--- q80_ReverseInteger.java
+++ q80_ReverseInteger.java
@@ -3,16 +3,16 @@ public class q80_ReverseInteger {
     public static void main(String[] args) {
         int x = -1234;
         System.out.println(reverse(x));
+        System.out.println(reverse2(x));
     }
 
     public static int reverse(int x) {
-
         // Brute force
         // TC = O(1)
         // SC = O(1)
 
-        // 2^31     = 2147483648
-        // 2^31 - 1 = 2147483647
+        // -2^31     = -2147483648
+        // 2^31 - 1  =  2147483647
 
         int original = x;                   // save original value of x
         x = Math.abs(x);                    // take only +ve value
@@ -30,4 +30,30 @@ public class q80_ReverseInteger {
         return (int) result;
     }
 
+    public static int reverse2(int x) {
+        // Iteration
+        // TC = O()
+        // SC = O()
+
+        final int MIN = -2147483648;
+        final int MAX = 2147483647;
+        int result = 0;
+
+        while (x != 0) {
+            int digit = x % 10;                             // take the value
+            x = x / 10;                                     // divide x / 10
+
+            // Condition check for limits
+            if (result > MAX/10 || (result == MAX/10 && digit > MAX % 10) ) {           // doing MAX/10 --> check if (res * 10) + digit -- exceeds limit
+                return 0;
+            }
+            if (result < MIN/10 || ((result == MIN/10 && digit < MIN%10)) ) {
+                return 0;
+            }
+
+            result = (result * 10) + digit;                 // add that value*10 to result
+        }
+
+        return result;
+    }
 }
